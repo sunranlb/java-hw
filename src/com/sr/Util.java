eabd8d26f2570a6a5cf873c7d651738716bcde84
sr <sunranlb@qq.com>
2022-09-16 11:19:01 +0800

    makelist

---
+++ src/com/sr/Util.java
@@ -0,0 +1,20 @@
+package com.sr;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Util {
+    public static List<Integer> makeList(int... a) {
+        List<Integer> res = new ArrayList<>();
+        for (int i : a) {
+            res.add(i);
+        }
+        return res;
+    }
+
+    public static List<List<Integer>> makeListList(List<Integer>... a) {
+        return new ArrayList<>(Arrays.asList(a));
+    }
+
+}
