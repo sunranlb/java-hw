67a25cc82acb83d210aed6e77c1650eccb4da441
sr <sunranlb@qq.com>
2022-09-18 17:34:38 +0800

    readfile

--- src/com/sr/Main.java
+++ src/com/sr/Main.java
@@ -1,11 +1,13 @@
 package com.sr;
 
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Main m = new Main();
+
 
     }
 }
--- src/com/sr/U.java
+++ src/com/sr/U.java
@@ -1,5 +1,8 @@
 package com.sr;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,4 +20,36 @@ public class U {
         return new ArrayList<>(Arrays.asList(a));
     }
 
+    public static Object[] readInputFromFile() {
+        Object[] res = null;
+        try {
+            FileInputStream inputStream = new FileInputStream("fi/input.txt");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            List<Object> r = new ArrayList<>();
+            String str = null;
+            List<Integer> ints = new ArrayList<>();
+            while ((str = bufferedReader.readLine()) != null) {
+                str = str.substring(1, str.length() - 1);
+                String[] split = str.split(",");
+                for (String s : split) {
+                    ints.add(Integer.parseInt(s));
+                }
+            }
+            int[] rints = new int[ints.size()];
+            for (int i = 0; i < rints.length; i++) {
+                rints[i] = ints.get(i);
+            }
+            r.add(rints);
+
+            res = new Object[r.size()];
+            res[0] = r.get(0);
+
+            inputStream.close();
+            bufferedReader.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        return res;
+    }
 }
